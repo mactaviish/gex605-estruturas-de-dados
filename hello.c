0c415e76fda10ccbd2d93ec056131f4b17e650d7
Leonardo Dalla Corte <37817455+mactaviish@users.noreply.github.com>
2022-05-11 21:42:37 -0300

    aula1

--- hello.c
+++ hello.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main() {
-    printf("Início de Estruturas de Dados");
+    int valor;
 
+    printf("Digite um inteiro: ");
+    scanf("%d", &valor);
+
+    printf("\nVocê digitou: %d", valor);
     return 0;
 }
