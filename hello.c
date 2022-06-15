6582a9166936e267e28b000060e9a847a5f14452
Leonardo Dalla Corte <37817455+mactaviish@users.noreply.github.com>
2022-06-14 22:01:15 -0300

    recursividade

--- hello.c
+++ hello.c
@@ -21,3 +21,43 @@ typedef struct {
     int mes;
     int ano;
 } Data;
+
+// Exercício 1
+#include <stdio.h>
+
+int potencia(int base, int expoente) {
+	if (expoente == 0)
+		expoente = 1;
+	else
+	if (expoente == 1)
+		expoente = base
+	else 
+		base = base * potencia(base, expoente - 1);
+	
+    printf("%d", valor);
+	return base;
+}
+
+int main() {
+	int valor = potencia(4, 4);
+
+	return 0;
+}
+
+// Exercício 3
+#include <stdio.h>
+
+int mdc(int m, int n) {
+	if (n == 0)
+		m;
+	else
+		m = mdc(n, m%n);
+	return m;
+}
+
+int main() {
+	int valor = mdc(12, 18);
+
+    printf("\n%d", valor);
+	return 0;
+}
