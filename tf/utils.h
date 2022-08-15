fb76634d2bb413fbea840033d5af1c26a3f394fa
Leonardo Dalla Corte <37817455+mactaviish@users.noreply.github.com>
2022-08-15 19:51:00 -0300

    https://trello.com/c/ZnRS1lIF/4-criar-a-estrutura-bebida

--- tf/utils.h
+++ tf/utils.h
@@ -2,3 +2,14 @@ typedef struct {
     char nome[50];
     int cnpj;
 } Empresa;
+
+struct _bebida {
+    int codigo;
+    char nome[50];
+    int conteudo;
+    double preco;
+    int estoque;
+    struct _bebida *prev;
+    struct _bebida *next;
+};
+typedef struct _bebida Bebida;
