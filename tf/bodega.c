edf6eda3afe25991f609bfdef4fca96483c1c73d
Leonardo Dalla Corte <37817455+mactaviish@users.noreply.github.com>
2022-08-15 20:11:51 -0300

    https://trello.com/c/gNDgQk7E/8-criar-o-menu-interativo

--- tf/bodega.c
+++ tf/bodega.c
@@ -1,7 +1,20 @@
 #include <stdio.h>
 #include "utils.h"
 
+void printMenu() {
+    system(“clear”);
+    printf("Escolha uma das opções abaixo:\n");
+    printf("[ 1 ] Cadastrar bebida\n");
+    printf("[ 2 ] Mostrar bebidas\n");
+    printf("[ 3 ] Comprar bebida\n");
+    printf("[ 4 ] Vender bebida\n");
+    printf("[ 5 ] Cadastrar cliente\n");
+    printf("[ 6 ] Mostrar clientes\n");
+    printf("[ 0 ] Sair do sistema\n");
+}
+
 int main() {
+    printMenu();
 
     return 0;
 }
